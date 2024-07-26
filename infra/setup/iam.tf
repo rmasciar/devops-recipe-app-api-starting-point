f0fb319f48c7a7041604befc5d6e831b99bbcb7d
DarumaDevelopment <develop@darumasoft.com>
2024-07-26 09:01:50 +0200

    ecs

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -130,6 +130,7 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:AttachInternetGateway",
       "ec2:ModifyVpcAttribute",
       "ec2:RevokeSecurityGroupIngress",
+      "ec2:DescribeAvailabilityZones"
     ]
     resources = ["*"]
   }
@@ -161,7 +162,9 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance"
+      "rds:ModifyDBInstance",
+      "rds:ModifyDBSubnetGroup",
+      "rds:AddTagsToResource"
     ]
     resources = ["*"]
   }
@@ -176,6 +179,30 @@ resource "aws_iam_policy" "rds" {
 resource "aws_iam_user_policy_attachment" "rds" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.rds.arn
+}
+
+data "aws_iam_policy_document" "service_linked_rds" {
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:CreateServiceLinkedRole"]
+    resources = ["arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS"]
+    condition {
+      test     = "StringLike"
+      variable = "iam:AWSServiceName"
+      values   = ["rds.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "service_linked_rds" {
+  name        = "${aws_iam_user.cd.name}-service_linked_rds"
+  description = "Allow Amazon RDS to call AWS services on behalf of your DB instances."
+  policy      = data.aws_iam_policy_document.service_linked_rds.json
+}
+
+resource "aws_iam_user_policy_attachment" "service_linked_rds" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.service_linked_rds.arn
 }
 
 #########################
